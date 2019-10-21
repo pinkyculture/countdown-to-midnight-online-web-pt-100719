ab95098ad4278e55ba5c538fb7b9c0c796756d09
daniel arzayus <randomblue1@yahoo.com>
2019-10-21 01:21:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
+#return "HAPPY NEW YEAR!"
 
-def countdown
+def countdown(second)
+   
+  number = 1
+  while number < 10 
+  number -= 1 
+  puts "#{number}SECOND(S)!""
+  
+end
 end
